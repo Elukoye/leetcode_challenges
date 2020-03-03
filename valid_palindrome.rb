f2a4e4011300244b56f14c7916440c7610cf9d21
Elukoye <lukoyedith@gmail.com>
2020-03-03 18:35:30 +0300

    added rspec tests to the solution

---
+++ valid_palindrome.rb
@@ -0,0 +1,17 @@
+
+def is_palindrome?(s)
+    if s.empty?
+      puts true
+    elsif s.gsub(/\W/,"").downcase == s.gsub(/\W/,"").downcase.reverse
+      puts true 
+    else 
+      puts false 
+    end    
+end
+  
+   
+puts is_palindrome?("A man, a plan,  a canal: Panama")
+puts is_palindrome?("")
+puts is_palindrome?("race a car")
+
+
