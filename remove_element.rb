56d369e2a401b84a47778bdc96530bf04c25fa1b
Elukoye <lukoyedith@gmail.com>
2020-03-19 14:12:41 +0300

    modified :remove_element.rb

--- remove_element.rb
+++ remove_element.rb
@@ -8,7 +8,7 @@
 # end
 
 def remove_val(nums,val)
-    puts 0 if nums.length == 0 || !nums.include?(val)
+    puts 0 if nums.length == 0 
     nums.each_with_index do |el,idx|
          if el == val
             nums[idx] = nil
@@ -17,4 +17,4 @@ def remove_val(nums,val)
     nums.compact!
     puts nums.length
 end
-puts remove_val( [0,1,2,2,3,0,4,2],0)
+puts remove_val( [0,1,2,2,3,0,4,2],3)
