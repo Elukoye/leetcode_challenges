0806564934ccf338b6007878e5846d10215f08f6
Elukoye <lukoyedith@gmail.com>
2020-03-19 13:57:20 +0300

    rspec test for remove element challenge

---
+++ spec/remove_element.rb
@@ -0,0 +1,11 @@
+require './remove_element'
+
+RSpec.describe '#remove_element' do
+    it 'removes dups from array & returns new array length' do
+        nums = [0,1,2,2,3,0,4,2]
+        val = 2
+       expect do 
+        remove_val(nums,val)
+       end.to output("5\n").to_stdout
+    end
+end
