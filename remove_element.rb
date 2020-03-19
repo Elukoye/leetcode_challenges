71d1bfb66636021d7e93b00d5d1171421cb45e0d
Elukoye <lukoyedith@gmail.com>
2020-03-19 13:55:26 +0300

    added leetcode solution to remove element solution

---
+++ remove_element.rb
@@ -0,0 +1,20 @@
+# def remove_val(nums,val)
+#     puts 0 if !nums.include?(val) || nums.empty?
+#     while nums.include?(val) do
+#         nums.delete(val)
+#         puts nums.length
+#     end
+    
+# end
+
+def remove_val(nums,val)
+    puts 0 if nums.length == 0 || !nums.include?(val)
+    nums.each_with_index do |el,idx|
+         if el == val
+            nums[idx] = nil
+         end
+    end 
+    nums.compact!
+    puts nums.length
+end
+puts remove_val( [0,1,2,2,3,0,4,2],0)
