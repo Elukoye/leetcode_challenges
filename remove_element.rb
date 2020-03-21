9573eb982cbb805abf88427a2341b8308fed31f0
Elukoye <lukoyedith@gmail.com>
2020-03-21 14:03:50 +0300

    modified remove element.rb & its spec file

--- remove_element.rb
+++ remove_element.rb
@@ -8,13 +8,13 @@
 # end
 
 def remove_val(nums,val)
-    puts 0 if nums.length == 0 
+    return 0 if nums.length == 0 
     nums.each_with_index do |el,idx|
          if el == val
             nums[idx] = nil
          end
     end 
     nums.compact!
-    puts nums.length
+    nums.length
 end
 puts remove_val( [0,1,2,2,3,0,4,2],3)
--- spec/remove_element.rb
+++ spec/remove_element.rb
@@ -4,17 +4,15 @@ RSpec.describe '#remove_element' do
     it 'removes dups from array & returns new array length' do
         nums = [0,1,2,2,3,0,4,2]
         val = 2
-       expect do 
-        remove_val(nums,val)
-       end.to output("5\n").to_stdout
+        expect(remove_val(nums,val)).to eq(5)
+       
     end
 
     it 'returns 0 when given an empty array' do
         nums = []
         val = 9
-        expect do 
-            remove_val(nums,val)
-        end.to output("0\n0\n").to_stdout
+        expect(remove_val(nums,val)).to eq(0)
+      
     end
 
 end
