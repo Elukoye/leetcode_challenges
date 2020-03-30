c7dcbb4cba414292c0cea32a60557db38d9b505b
Elukoye <lukoyedith@gmail.com>
2020-03-30 14:15:21 +0300

    added leetcode add binary solution and rspec tests

---
+++ max_subarray.rb
@@ -0,0 +1,11 @@
+# return 0 if arr.length < 1
+# return nums[0] if arr.length == 1
+# set counter to 0
+# while the counter is less than 2 do
+#find  first max number in the array
+# store it in variable max_1 
+# delete max_1 from array
+# find new max number in array
+#store it in variable max_2 
+# end the while loop
+# return sum of max_1 and max_2 
