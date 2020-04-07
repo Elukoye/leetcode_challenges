43889aee2dcd6855df58bd12c28a978c1eadc3b9
Elukoye <lukoyedith@gmail.com>
2020-04-07 16:43:26 +0300

    maximum subarray challenge on leetcode

--- max_subarray.rb
+++ max_subarray.rb
@@ -1,13 +1,16 @@
-# return 0 if arr.length < 1
-# return nums[0] if arr.length == 1
-# set counter to 0
-# while the counter is less than 2 do
-#find  first max number in the array
-# store it in variable max_1 
-# delete max_1 from array
-# find new max number in array
-#store it in variable max_2 
-# end the while loop
-# return sum of max_1 and max_2 
 
+# https://www.youtube.com/watch?v=2MmGzdiKR9Y&t=488s
+# https://www.youtube.com/watch?v=86CQq3pKSUw&t=378s
 
+def max_sub_array(nums)
+    current_sum = nums[0]
+    current_max = current_sum
+    
+    nums[1..-1].each do |num|
+        current_sum = [current_sum + num , num].max
+        if current_sum > current_max
+            current_max = current_sum
+        end
+    end
+    current_max
+end
