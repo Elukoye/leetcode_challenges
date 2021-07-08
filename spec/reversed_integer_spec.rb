02ddcd744beacdde36f97c8acc182162f03ad0ac
Emogene Lukoye <lukoyedith@gmail.com>
2021-07-08 11:58:42 +0300

    Delete spec directory

--- spec/reversed_integer_spec.rb
+++
@@ -1,8 +0,0 @@
-require './reversed_integer.rb'
-
-RSpec.describe "#reverse" do
-    it ' returns a positive reversed integer' do
-        x = 231
-        expect(reverse(x)).to eq(132)
-    end
-end
